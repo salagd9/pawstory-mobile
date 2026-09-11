267d7294286d0981f1fba4833cd6e5eb6984735e
salagd9 <salagd9@gmail.com>
2026-09-11 18:42:51 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event){
+});
