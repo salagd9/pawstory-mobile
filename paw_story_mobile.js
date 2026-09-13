93a70113d722bcc9317389de283eada53b9c947c
PawStory <salagd9@gmail.com>
2026-09-13 13:01:40 +0900

    Add files via upload

--- paw_story_mobile.js
+++ paw_story_mobile.js
@@ -4,7 +4,7 @@ document.body.innerHTML =
 
 '<div id="mainMenu">' +
   '<div id="mainScreen">' +
-'<img id="mainImage" src="images/main.png">' +
+'<img id="mainImage" src="images/images/main.png">' +
     '<button id="mainStart"></button>' +
   '</div>' +
 '</div>' +
